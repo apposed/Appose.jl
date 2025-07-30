ef8e861b8579411e5eb07189760c5cda24ba1d1b
Documenter.jl <documenter@juliadocs.github.io>
2025-07-30 16:31:40 +0000

    build based on aad9e21

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Appose","page":"Home","title":"Appose","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Appose.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
