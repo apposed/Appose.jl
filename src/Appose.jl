dee18b8b2b290b7cd62fa9a864af01836e4fe43b
Mark Kittisopikul <markkitt@gmail.com>
2025-07-30 13:57:13 -0400

    Add working SharedArray testing code

--- src/Appose.jl
+++ src/Appose.jl
@@ -1,5 +1,110 @@
 module Appose
 
-# Write your package code here.
+using JSON3
+using UUIDs
+using SharedArrays
+# TODO: Make into an extension package
+using CondaPkg
 
+function launch_python_apposed()
+    python_command = "import appose.python_worker; appose.python_worker.main()"
+    to_python = Pipe()
+    from_python = Pipe()
+    err_python = Pipe()
+    CondaPkg.withenv() do
+        pl = pipeline(
+            `python -c $python_command`,
+            stdin=to_python,
+            stdout=from_python,
+            stderr=err_python
+        )
+        p = run(pl, wait=false)
+    end
+    Threads.@spawn while true
+        println(stderr, readline(err_python))
+    end
+    output = Channel()
+    Threads.@spawn while true
+        push!(output, JSON3.read(readline(from_python)))
+    end
+    input = Channel()
+    Threads.@spawn while true
+        command = take!(input)
+        println(to_python, JSON3.write(command))
+    end
+    return (; input, output)
 end
+
+function appose_execute(script, input=Dict{String,Any}())
+    return Dict{String, Any}(
+        "task" => string(uuid4()),
+        "requestType" => "EXECUTE",
+        "script" => script,
+        "inputs" => input
+    )
+end
+
+"""
+create_python_shared_memory(
+    input::Channel,
+    output::Channel,
+    dtype::Type,
+    size::Dims
+)
+
+Function to test creation of Python Shared Memory
+"""
+function create_python_shared_memory(
+    input::Channel,
+    output::Channel,
+    dtype::Type,
+    size::Dims
+)
+    # Transform data for Python script
+    python_rsize = sizeof(dtype) * prod(size)
+    # TODO: Map Julia types to Numpy types via PythonCall?
+    python_dtype = lowercase(string(dtype))
+    python_size = reverse(size)
+    python_zeros = zero.(size)
+
+    # Indentation matters here
+    python_command = """
+    import appose
+    shm = appose.SharedMemory(create = True, rsize=$python_rsize)
+    data = appose.NDArray("$python_dtype", $python_size, shm)
+    data.ndarray()[$python_zeros] = 123 # testing
+    task.outputs["result"] = data
+    """
+    put!(input, appose_execute(python_command))
+
+    # Handle LAUNCH message
+    launch = take!(output)::JSON3.Object
+    if launch["responseType"] == "FAILURE"
+        error("Appose Failure: " * launch["error"])
+    elseif launch["responseType"] == "LAUNCH"
+        @info "Launched" launch["task"]
+    else
+        error("Unknown Appose Response: " * string(launch))
+    end
+
+    # Handle COMPLETION message
+    completion = take!(output)
+    if completion["responseType"] == "FAILURE"
+        error("Appose Failure: " * completion["error"])
+    elseif completion["responseType"] == "COMPLETION"
+        result = completion.outputs.result
+    else
+        error("Unknown Appose Response: " * string(launch))
+    end
+
+    # TODO: Generalize to Windows
+    arr = SharedArray{dtype}("/dev/shm/" * result.shm.name, size)
+    return arr
+end
+
+function create_python_shared_memory(dtype, size)
+    in, out = launch_python_apposed()
+    return create_python_shared_memory(in, out, dtype, size)
+end
+
+end # module Appose
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using Appose
+using SharedArrays
 using Test
 
-@testset "Appose.jl" begin
-    # Write your tests here.
+@testset "Appose.jl Shared Arrays" begin
+    in, out = Appose.launch_python_apposed()
+    for T in (Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64),
+        _size in ((2,), (2,3), (2,3,4), (2,3,4,5))
+        shared_arr = Appose.create_python_shared_memory(in, out, T, _size)
+        @test size(shared_arr) == _size
+        @test eltype(shared_arr) == T
+        @test typeof(shared_arr) == SharedArray{T, length(_size)}
+        @test first(shared_arr) == 123
+    end
 end
