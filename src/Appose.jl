9aec7578b0bceb3f077e25ff515d437da0c07396
brp-optics <10531278+brp-optics@users.noreply.github.com>
2026-06-20 09:23:35 +0900

    Add async Service for processing responses with UUID based dispatching
    The previous version of Appose.jl was a proof of concept
    assuming sequential worker responses. By adding Appose.Service which
    runs the input and output, we are able to direct responses from multiple
    appose task in the same worker process without interference.
    
    - start_python_service is now the entry way into appose python workers
    - Service and Task structs added
    - dispatch_loop! processes responses into matching tasks
    - execute! sends tasks to the service.

--- src/Appose.jl
+++ src/Appose.jl
@@ -1,5 +1,7 @@
 module Appose
-export TaskStatus, INITIAL, QUEUED, RUNNING, COMPLETE, CANCELLED, FAILED, CRASHED, is_error, is_finished
+export TaskStatus, INITIAL, QUEUED, RUNNING, COMPLETE, CANCELED, FAILED, CRASHED, is_error, is_finished, wait_for, set_status!, Service, start_python_service, register_task!, lookup_task, execute!, dispatch_loop!, handle_response!, close!
+
+# We don't export Task, because that collides with Base.Task. Reference Appose.Task instead.
 
 using JSON3
 using UUIDs
@@ -13,17 +15,211 @@ using CondaPkg
     QUEUED
     RUNNING
     COMPLETE
-    CANCELLED
+    CANCELED
     FAILED
     CRASHED
 end
 
-is_error(s::TaskStatus)::Bool = s in (CANCELLED, FAILED, CRASHED)
+is_error(s::TaskStatus)::Bool = s in (CANCELED, FAILED, CRASHED)
 is_finished(s::TaskStatus)::Bool = s == COMPLETE || is_error(s)
 status_priority(s::TaskStatus)::Int = is_error(s) ? 4 : Int(s)
 
 
+mutable struct Task
+    ## An Appose *task* is an asynchronous operation performed by its
+    ## associated Appose *service*. It is analogous to an asyncio.Future.
+    ## This struct mirrors the python task in service.py.
+    # service
+    uuid::String
+    script::String
+    # queue
+    inputs::Dict{String, Any}
+    outputs::Dict{String, Any}
+    status::TaskStatus
+    error::Union{String, Nothing}
+    listeners::Vector{Function}
+    cv::Threads.Condition
+end
 
+function Task(script::String;
+              inputs::Dict{String, Any} = Dict{String, Any}(),
+              uuid::String = string(uuid4()))
+    Task(uuid, script, inputs, Dict{String, Any}(), INITIAL,
+         nothing, Function[], Threads.Condition())
+end
+
+## The Appose Service allows us to dispatch on the UUIDs of tasks,
+## and run tasks asychronously. This is important because the
+## worker can be multithreaded.
+mutable struct Service
+    ## An Appose *service* provides access to a linked Appose *worker* running
+    ## in a different process. Using the service, programs create Appose *tasks*
+    ## that run asynchronously in the worker process, which notifies the
+    ## service of updates via communication over pipes (stdin and stdout).
+    process::Base.Process
+    input::Channel
+    output::Channel
+    tasks::Dict{String, Task}
+    tasks_lock::ReentrantLock
+    listener_task::Union{Base.Task, Nothing}
+end
+
+function Service(process::Base.Process, input::Channel, output::Channel)
+    Service(process, input, output, Dict{String, Task}(), ReentrantLock(), nothing)
+end
+
+function set_status!(t::Task, s::TaskStatus)
+    lock(t.cv) do
+        t.status = s
+        if is_finished(s)
+            notify(t.cv; all=true)
+        end
+    end
+end
+
+function wait_for(t::Task)::Task
+    ## Waits for this task to complete.
+    ## Returns this task for method chaining.
+    ## [TODO] Raises error if task fails, is canceled, or crashes.
+    lock(t.cv) do
+        while !is_finished(t.status)
+            wait(t.cv)
+        end
+        if t.status != COMPLETE && is_error(t.status)
+            #raise_error
+        end
+    end
+    return t
+end
+
+function listen!(t::Task, listener::Function)
+    # Registers a callback to be notified when task updates status.
+    lock(t.cv) do
+        if t.status != INITIAL
+            error("In Appose.listen!: Attempting to add listener while Appose Task is not in INITIAL state")
+        else
+            push!(t.listeners, listener)
+        end
+    end
+end
+
+function start_python_service()::Service
+    python_command = "import appose.python_worker; appose.python_worker.main()"
+    to_python = Pipe()
+    from_python = Pipe()
+    err_python = Pipe()
+    p = CondaPkg.withenv() do
+        pl = pipeline(
+            `python -c $python_command`,
+            stdin=to_python,
+            stdout=from_python,
+            stderr=err_python
+        )
+        p = run(pl, wait=false)
+    end
+    
+    # python stderr to Julia stderr
+    errormonitor(
+        Threads.@spawn while !eof(err_python)
+        println(stderr, readline(err_python))
+        end
+    )
+    
+    output = Channel()  # __Claude recommended Channel{Any}(32), but I don't understand why.
+    errormonitor(
+        Threads.@spawn while !eof(from_python)
+            l = readline(from_python)
+            isempty(strip(l)) && continue
+            local parsed
+            try
+               parsed = JSON3.read(l)
+            catch e
+               println(stderr, "$e: Invalid read from python: $l")
+               # TODO: python collects invalid lines for diagnostics
+               continue
+            end
+            push!(output, parsed)
+        end
+    )
+    
+    input = Channel()
+    errormonitor(
+        Threads.@spawn while true
+        command = take!(input)
+        println(to_python, JSON3.write(command))
+        end
+    )
+    
+    svc = Service(p, input, output)
+    lt = errormonitor(
+        Threads.@spawn dispatch_loop!(svc)
+    )
+    svc.listener_task = lt 
+    return svc
+end
+
+function dispatch_loop!(svc::Service)
+    # Pulls responses from the service and routes them to the appropirate tasks.
+    # Runs in its own thread.
+    while true
+        local response, uuid
+        try
+            response = take!(svc.output) # output is JSON3.Object
+            uuid = response[:task]
+            if uuid === nothing
+                continue
+            end            
+        catch e # Did channel close?
+            e isa InvalidStateException ? break : rethrow()
+        end
+        task = lock(svc.tasks_lock) do
+            get(svc.tasks, uuid, nothing)
+        end
+        if task === nothing
+            continue
+        end
+        handle_response!(task, response)
+    end
+end
+
+function register_task!(svc::Service, task::Task)
+    ## Insert a task into the service's registry under its uuid.
+    ## Do so safely by locking the service.
+    lock(svc.tasks_lock) do
+        svc.tasks[task.uuid] = task
+    end
+end
+
+function lookup_task(svc::Service, uuid::AbstractString)::Union{Task, Nothing}
+    ## Return task from service given its uuid.
+    lock(svc.tasks_lock) do
+        get(svc.tasks, String(uuid), nothing)
+    end
+end
+
+function execute!(svc::Service, task::Task)
+    ## Submit a new task to the service/worker
+    ## Assumes the task has already been constructed with Task().
+    ## We add the task to the service first to avoid dispatching a uuid that isn't in the table later.
+    ## Todo: make a wrapper version which initializes the task given a script and inputs.
+    register_task!(svc, task)
+    set_status!(task, QUEUED)
+    request = Dict(
+        "task" => task.uuid,
+        "requestType" => "EXECUTE",
+        "script" => task.script,
+        "inputs" => task.inputs,
+    )
+    put!(svc.input, request)
+    return task
+end
+
+"""
+   launch_python_apposed()::(; ::Channel, ::Channel)
+
+Deprecating or rewriting this interface soon.
+Please use Appose.Service for new code.
+"""
 function launch_python_apposed()
     python_command = "import appose.python_worker; appose.python_worker.main()"
     to_python = Pipe()
@@ -51,18 +247,73 @@ function launch_python_apposed()
         println(to_python, JSON3.write(command))
     end
     return (; input, output)
-    # TODO: no UUID dispatch.
 end
 
-function appose_execute(script, input=Dict{String,Any}())
+
+function appose_execute(script, inputs=Dict{String,Any}())::Dict #::Task
+    # Included for backwards compatibility during refactor. Previously returned Dict().
+    Task(script; inputs=inputs) # to do: remove Dict block below.
     return Dict{String, Any}(
         "task" => string(uuid4()),
         "requestType" => "EXECUTE",
         "script" => script,
-        "inputs" => input
+        "inputs" => inputs
     )
 end
 
+function handle_response!(task::Task, response)
+    ## Route a single response by its type,
+    ## Updates the task
+    rtype = get(response, :responseType, nothing)
+    if rtype == nothing
+        return
+    end
+    if rtype == "LAUNCH"
+        set_status!(task, RUNNING)
+    elseif rtype=="UPDATE"
+        ## TODO: python runs listeners
+    elseif rtype=="COMPLETION"
+        d = get(response, :outputs, nothing)
+        if !(d === nothing)
+            lock(task.cv) do
+                for (key, value) in d
+                    task.outputs[String(key)] = value
+                end
+            end
+        end
+        # Order matters: set_status! calls notify(task.cv),
+        # so set all task non-statuses first.
+        set_status!(task, COMPLETE)
+    elseif rtype == "FAILURE"
+        lock(task.cv) do         
+            task.error = String(get(response, :error, nothing))
+        end
+        # Order matters: set_status! calls notify(task.cv),
+        # so set all task non-statuses first.
+        set_status!(task, FAILED)
+    elseif rtype == "CANCELATION"
+        set_status!(task, CANCELED)
+    end
+    for listener in task.listeners
+        listener(response)
+    end
+    if is_finished(task.status)
+        lock(task.cv) do
+            notify(task.cv)
+        end
+    end
+end
+
+"""
+    close!(Appose.Service)
+
+Closes the service without waiting for tasks to complete.
+"""
+
+function close!(svc::Service)
+   ## TODO 
+end
+    
 """
 create_python_shared_memory(
     input::Channel,
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,116 @@
 using Appose
 using SharedArrays
 using Test
+using Random
 
-@testset "Appose.jl Shared Arrays" begin
-    in, out = Appose.launch_python_apposed()
-    for T in (Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64, Float32, Float64),#, Bool),
-        _size in ((2,), (2,3), (2,3,4), (2,3,4,5))
-        shared_arr = Appose.create_python_shared_memory(in, out, T, _size)
-        @test size(shared_arr) == _size
-        @test eltype(shared_arr) == T
-        @test typeof(shared_arr) == SharedArray{T, length(_size)}
-        @test first(shared_arr) == 123
+@testset "Appose.jl Python Service" begin
+    svc = start_python_service()
+    t = Appose.Task("task.outputs['answer'] = 6*7")
+    execute!(svc,t)
+    wait_for(t) # Test actually fails if you don't wait
+    @test t.outputs["answer"] == 42
+
+    # Test concurrent wait_for() on a single task.
+    t = Appose.Task("task.outputs['nohang'] = 1")
+    Appose.execute!(svc, t)
+    done = Threads.Atomic{Bool}(false)
+    waiter = Threads.@spawn begin
+        Appose.wait_for(t)
+        done[] = true
+    end
+    for _ in 1:10
+        done[] && break
+        sleep(0.01)
+    end
+    @test done[]  # False means it hung
+    
+    # 50 concurrent tasks on same service, confirm uuid dispatch
+    tasks = [ Appose.Task(
+        "import time, random; time.sleep(0.05*random.random()); task.outputs['v']=$i*2")
+              for i in 1:50 ]
+    for t in tasks
+        execute!(svc,t)
+    end
+    for t in tasks
+        wait_for(t)
+    end
+    wait_for(tasks[1]) # Shouldn't hang.
+    for i in 1:length(tasks)
+        @test tasks[i].outputs["v"] == i*2
+    end
+
+    # Test mix of success and failure
+    tasks = [ Appose.Task(
+        "import time, random; time.sleep(0.05*random.random()); task.outputs['sf']= 'even' if $i % 2 == 0 else odd") #Deliberate NameError
+              for i in 1:50 ]
+    for t in tasks
+        execute!(svc,t)
+    end
+    for t in tasks
+        wait_for(t)
+    end
+    wait_for(tasks[1]) # Shouldn't hang.
+    for i in 1:length(tasks)
+        if i % 2 == 0
+            @test tasks[i].outputs["sf"] == "even"
+        else
+            @test occursin("NameError", tasks[1].error)
+        end
+        
+    end
+
+    
+    
+    # This tests closing the service. Run it last.
+    # Not yet implemented.
+    # closedtask = Appose.Task("import time; time.sleep(1); task.outputs['oops'] = 'finished'")
+    #execute!(closedtask)
+    #close!(svc)
+    #wait_for(closedtask)
+    
+end
+
+
+@testset "Many tasks complete" begin
+    tasks = [Appose.Task("noop") for _ in 1:50]
+    @async begin
+        for t in shuffle(tasks)
+            Appose.set_status!(t, Appose.COMPLETE)
+        end
+    end
+    for t in tasks
+        Appose.wait_for(t)
+        @test t.status == Appose.COMPLETE
     end
 end
+
+@testset "Legacy concurrent tasks on same worker" begin
+    in, out = Appose.launch_python_apposed()
+
+    t1 = Threads.@spawn Appose.create_python_shared_memory(in, out, Int32, (4,))
+    t2 = Threads.@spawn Appose.create_python_shared_memory(in, out, Int32, (4,))
+    a = fetch(t1)
+    b = fetch(t2)
+    @test first(a) == 123
+    @test first(b) == 123
+end
+
+
+@testset "Legacy Appose.jl" begin
+    @testset "Shared Arrays" begin
+        in, out = Appose.launch_python_apposed()
+        for T in (Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64, Float32, Float64),#, Bool),
+            _size in ((2,), (2,3), (2,3,4), (2,3,4,5))
+            shared_arr = Appose.create_python_shared_memory(in, out, T, _size)
+            @test size(shared_arr) == _size
+            @test eltype(shared_arr) == T
+            @test typeof(shared_arr) == SharedArray{T, length(_size)}
+            @test first(shared_arr) == 123
+        end
+    end
+end
+
+
 
 @testset "TaskStatus enum" begin
     @test is_finished(COMPLETE)
@@ -22,13 +120,33 @@ end
     @test !is_error(COMPLETE)
 end
 
+@testset "Task struct and wait_for" begin
+    
+    
+    # Task with completion
+    t = Appose.Task("#This task completes")
+    @test t.status == Appose.INITIAL
+    @test t.uuid != ""
+    @test isempty(t.inputs)
+    @test isempty(t.outputs)
 
-@testset "Appose.jl Concurrent tasks on same worker" begin
-    in, out = Appose.launch_python_apposed()
-    t1 = Threads.@spawn Appose.create_python_shared_memory(in, out, Int32, (4,))
-    t2 = Threads.@spawn Appose.create_python_shared_memory(in, out, Int32, (4,))
-    a = fetch(t1)
-    b = fetch(t2)
-    @test first(a) == 123
-    @test first(b) == 123
+    @async begin
+        sleep(0.1)
+        Appose.set_status!(t, Appose.COMPLETE)
+    end
+
+    Appose.wait_for(t)
+    @test t.status == Appose.COMPLETE
+
+    # Task with failure
+    t = Appose.Task("#This task fails")
+    @async begin
+        sleep(0.05)
+        t.error = "Task forced to fail."
+        Appose.set_status!(t, Appose.FAILED)
+    end
+    Appose.wait_for(t)
+    @test t.status == Appose.FAILED
+    @test t.error == "Task forced to fail."
 end
+
